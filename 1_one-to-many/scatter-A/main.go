18b2b18e592f2de56d4bcb0ede6ee120c8aab31b
Ray Hu <raymond.hu@imperial.ac.uk>
2018-09-26 13:59:21 +0100

    Continued.

--- 1_one-to-many/scatter-A/main.go
+++ 1_one-to-many/scatter-A/main.go
@@ -9,17 +9,11 @@ package main
 import (
 	"encoding/gob"
 
-	"github.com/rhu1/scribble-go-runtime/runtime/session2"
-	//"github.com/rhu1/scribble-go-runtime/runtime/transport2/shm"
-	"github.com/rhu1/scribble-go-runtime/runtime/transport2/tcp"
-
+	"github.com/nickng/scribble-go-examples/scributil"
 	"github.com/nickng/scribble-go-examples/1_one-to-many/OneToMany/Scatter"
 	"github.com/nickng/scribble-go-examples/1_one-to-many/messages"
 	"github.com/nickng/scribble-go-examples/1_one-to-many/scatter"
 )
-
-//var _ = shm.Dial
-var _ = tcp.Dial
 
 func init() {
 	var data onetomany.Data
@@ -27,13 +21,8 @@ func init() {
 }
 
 func main() {
-	port := 33333
-	K := 2
+	_, dial, fmtr, port, K := scributil.ParseFlags()
+
 	p := Scatter.New()
-	dial := tcp.Dial
-	fmtr := func() session2.ScribMessageFormatter { return new(session2.GobFormatter) } 
-	/*// Not applicable to distributed scenario
-	dial := shm.Dial
-	fmtr := func() session2.ScribMessageFormatter { return new(session2.PassByPointer) }*/
 	scatter.Client_scatter(dial, fmtr, "localhost", port, p, K, 1)
 }
--- 1_one-to-many/scatter-B/main.go
+++ 1_one-to-many/scatter-B/main.go
@@ -11,17 +11,11 @@ import (
 	"encoding/gob"
 	"sync"
 
-	"github.com/rhu1/scribble-go-runtime/runtime/session2"
-	//"github.com/rhu1/scribble-go-runtime/runtime/transport2/shm"
-	"github.com/rhu1/scribble-go-runtime/runtime/transport2/tcp"
-
+	"github.com/nickng/scribble-go-examples/scributil"
 	"github.com/nickng/scribble-go-examples/1_one-to-many/OneToMany/Scatter"
 	"github.com/nickng/scribble-go-examples/1_one-to-many/messages"
 	"github.com/nickng/scribble-go-examples/1_one-to-many/scatter"
 )
-
-//var _ = shm.Listen
-var _ = tcp.Listen
 
 func init() {
 	var data onetomany.Data
@@ -29,16 +23,11 @@ func init() {
 }
 
 func main() {
-	port := 33333
-	K := 2
+	listen, _, fmtr, port, K := scributil.ParseFlags()
+
 	p := Scatter.New()  // FIXME: K should be param here?
 	wg := new(sync.WaitGroup)
 	wg.Add(2)
-	listen := tcp.BListen
-	fmtr := func() session2.ScribMessageFormatter { return new(session2.GobFormatter) } 
-	/*// Not applicable to distributed scenario
-	dial := shm.Dial
-	fmtr := func() session2.ScribMessageFormatter { return new(session2.PassByPointer) }*/
 	for i := 1; i <= K; i++ {
 		go scatter.Server_gather(listen, fmtr, port+i, p, K, i, wg)
 	}
