f3770b072a95f76b10f1abf1e1957d4cde83ef6f
Ray Hu <raymond.hu@imperial.ac.uk>
2018-09-26 13:55:32 +0100

    Continued.

--- 1_one-to-many/scatter-local/main.go
+++ 1_one-to-many/scatter-local/main.go
@@ -12,10 +12,10 @@ import (
 	"sync"
 	"time"
 
-	"github.com/rhu1/scribble-go-runtime/runtime/session2"
 	"github.com/rhu1/scribble-go-runtime/runtime/transport2/shm"
 	"github.com/rhu1/scribble-go-runtime/runtime/transport2/tcp"
 
+	"github.com/nickng/scribble-go-examples/scributil"
 	"github.com/nickng/scribble-go-examples/1_one-to-many/OneToMany/Scatter"
 	"github.com/nickng/scribble-go-examples/1_one-to-many/messages"
 	"github.com/nickng/scribble-go-examples/1_one-to-many/scatter"
@@ -29,25 +29,17 @@ func init() {
 	gob.Register(&data)
 }
 
+//..HERE: take CL arg for transport
 func main() {
-	port := 33333
-	K := 2
+	listen, dial, fmtr, port, K := scributil.ParseFlags()
+
 	p := Scatter.New()  // FIXME: K should be param here?
 	wg := new(sync.WaitGroup)
 	wg.Add(2)
-	/*/
-	listen := tcp.BListen
-	dial := tcp.Dial
-	fmtr := func() session2.ScribMessageFormatter { return new(session2.GobFormatter) } 
-	/*/
-	listen := shm.BListen
-	dial := shm.Dial
-	fmtr := func() session2.ScribMessageFormatter { return new(session2.PassByPointer) }
-	//*/
 	for i := 1; i <= K; i++ {
 		go scatter.Server_gather(listen, fmtr, port+i, p, K, i, wg)
 	}
-	time.Sleep(100 * time.Millisecond) //2017/12/11 11:21:40 cannot connect to 127.0.0.1:8891: dial tcp 127.0.0.1:8891: connectex: No connection could be made because the target machine actively refused it.
+	time.Sleep(100 * time.Millisecond)
 	scatter.Client_scatter(dial, fmtr, "localhost", port, p, K, 1)
 	wg.Wait()
 }
---
+++ scributil/scributil.go
@@ -0,0 +1,37 @@
+package scributil
+
+import (
+	"flag"
+	"log"
+
+	"github.com/rhu1/scribble-go-runtime/runtime/session2"
+	"github.com/rhu1/scribble-go-runtime/runtime/transport2"
+	"github.com/rhu1/scribble-go-runtime/runtime/transport2/shm"
+	"github.com/rhu1/scribble-go-runtime/runtime/transport2/tcp"
+)
+
+func ParseFlags() (func(int) (transport2.ScribListener, error),
+			func(host string, port int) (transport2.BinChannel, error),
+			func() session2.ScribMessageFormatter,
+			int, int) {
+	trnsprt := *flag.String("t", "tcp", "Transport: tcp/shm")
+  port:= *flag.Int("port", 33333, "base port for server sockets")
+	K := *flag.Int("K", 2, "K parameter value")
+  flag.Parse()
+	var listen func(int) (transport2.ScribListener, error)
+	var dial func(host string, port int) (transport2.BinChannel, error)
+	var fmtr func() session2.ScribMessageFormatter
+	switch trnsprt {
+	case "tcp":
+		listen = tcp.BListen
+		dial = tcp.Dial
+		fmtr = func() session2.ScribMessageFormatter { return new(session2.GobFormatter) } 
+	case "shm":
+		listen = shm.BListen
+		dial = shm.Dial
+		fmtr = func() session2.ScribMessageFormatter { return new(session2.PassByPointer) }
+	default:
+		log.Fatal("Unknown transport: ", trnsprt)
+	}
+	return listen, dial, fmtr, port, K
+}
