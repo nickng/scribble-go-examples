38a694e528645bed7f225f3cc181641d9c342d28
Nicholas Ng <nickng@nickng.io>
2018-10-04 01:55:22 +0100

    pget: Move implementation to subfolder

--- 9_pget/basic/main.go
+++ 9_pget/basic/main.go
@@ -1,5 +1,5 @@
-//go:generate scribblec-param.sh -d . PGet.scr -param Basic github.com/nickng/scribble-go-examples/9_pget/PGet -param-api M -param-api F -param-api S
-//go:generate scribblec-param.sh -d . PGet.scr -param Sync github.com/nickng/scribble-go-examples/9_pget/PGet -param-api A -param-api B
+//go:generate scribblec-param.sh -d ../ ../PGet.scr -param Basic github.com/nickng/scribble-go-examples/9_pget/PGet -param-api M -param-api F -param-api S
+//go:generate scribblec-param.sh -d ../ ../PGet.scr -param Sync github.com/nickng/scribble-go-examples/9_pget/PGet -param-api A -param-api B
 
 package main
 
