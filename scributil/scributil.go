e67ec0b6e32e928218790671d3b2a032b10c586f
Nicholas Ng <nickng@nickng.io>
2018-10-04 18:45:19 +0100

    scributil: return a ConnParam instead of tuple.

--- scributil/scributil.go
+++ scributil/scributil.go
@@ -1,3 +1,5 @@
+// Package scributil contains helper functions
+// for using the scribble-go runtime API.
 package scributil
 
 import (
@@ -10,29 +12,104 @@ import (
 	"github.com/rhu1/scribble-go-runtime/runtime/transport2/tcp"
 )
 
-func ParseFlags() (func(int) (transport2.ScribListener, error),
-			func(host string, port int) (transport2.BinChannel, error),
-			func() session2.ScribMessageFormatter,
-			int, int) {
-	trnsprt := flag.String("t", "tcp", "Transport: tcp/shm")  // N.B. cannot deref here -- gives default value before parsing
-  port:= flag.Int("port", 33333, "base port for server sockets")
-	K := flag.Int("K", 2, "K parameter value")
-  flag.Parse()
+// ConnParam is a connection parameter object.
+// Use methods provided to create new connections.
+type ConnParam interface {
+	Dial(host string, port int) (transport2.BinChannel, error)
+	Listen(port int) (transport2.ScribListener, error)
+	Formatter() session2.ScribMessageFormatter
 
-	var listen func(int) (transport2.ScribListener, error)
-	var dial func(host string, port int) (transport2.BinChannel, error)
-	var fmtr func() session2.ScribMessageFormatter
-	switch *trnsprt {
-	case "tcp":
-		listen = tcp.BListen
-		dial = tcp.Dial
-		fmtr = func() session2.ScribMessageFormatter { return new(session2.GobFormatter) } 
-	case "shm":
-		listen = shm.BListen
-		dial = shm.Dial
-		fmtr = func() session2.ScribMessageFormatter { return new(session2.PassByPointer) }
+	BasePort() int
+	Port(offset int) int
+	Param() int
+}
+
+type shmConn struct {
+	basePort int
+	paramVal int
+}
+
+func newShmConn(basePort int, K int) shmConn {
+	return shmConn{basePort: basePort, paramVal: K}
+}
+
+func (c shmConn) Dial(host string, port int) (transport2.BinChannel, error) {
+	return shm.Dial(host, port)
+}
+
+func (c shmConn) Listen(port int) (transport2.ScribListener, error) {
+	return shm.Listen(port)
+}
+
+func (c shmConn) Formatter() session2.ScribMessageFormatter {
+	return new(session2.PassByPointer)
+}
+
+func (c shmConn) BasePort() int       { return c.basePort }
+func (c shmConn) Port(offset int) int { return c.basePort + offset }
+func (c shmConn) Param() int          { return c.paramVal }
+
+type tcpConn struct {
+	basePort int
+	paramVal int
+}
+
+func newTcpConn(basePort int, K int) tcpConn {
+	return tcpConn{basePort: basePort, paramVal: K}
+}
+
+func (c tcpConn) Dial(host string, port int) (transport2.BinChannel, error) {
+	return tcp.Dial(host, port)
+}
+func (c tcpConn) Listen(port int) (transport2.ScribListener, error) {
+	return tcp.Listen(port)
+}
+
+func (c tcpConn) Formatter() session2.ScribMessageFormatter {
+	return new(session2.GobFormatter)
+}
+
+func (c tcpConn) BasePort() int       { return c.basePort }
+func (c tcpConn) Port(offset int) int { return c.basePort + offset }
+func (c tcpConn) Param() int          { return c.paramVal }
+
+// ServerConn expose only the server-side parameter of the ConnParam.
+type ServerConn interface {
+	Listen(port int) (transport2.ScribListener, error)
+	Formatter() session2.ScribMessageFormatter
+}
+
+// ClientConn expose only the client-side parameters of the ConnParam.
+type ClientConn interface {
+	Dial(host string, port int) (transport2.BinChannel, error)
+	Formatter() session2.ScribMessageFormatter
+}
+
+// ParseFlags parses command line flags and returns
+// a connection parameter object.
+func ParseFlags() (cparam ConnParam, K int) {
+	const (
+		tcpTran     = "tcp"
+		shmTran     = "shm"
+		defaultPort = 33333
+	)
+	var (
+		tran  string // transport
+		port  int    // base port
+		param int    // parameter value
+	)
+	flag.StringVar(&tran, "t", tcpTran, "transport: tcp/shm")
+	flag.IntVar(&port, "port", defaultPort, "base port for sever sockets")
+	flag.IntVar(&param, "K", 2, "K parameter value")
+	flag.Parse()
+
+	switch tran {
+	case tcpTran:
+		return newTcpConn(port, param), param
+	case shmTran:
+		return newShmConn(port, param), param
 	default:
-		log.Fatal("Unknown transport: ", trnsprt)
+		log.Fatalf("unrecognised transport: %s", tran)
 	}
-	return listen, dial, fmtr, *port, *K
+	return nil, param
 }
